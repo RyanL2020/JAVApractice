9c69e968841791a80ba9334e1099448695266de1
RL2020 <loc.fitness1@gmail.com>
2022-09-09 18:23:10 -0400

    Practice

---
+++ Main.java
@@ -0,0 +1,48 @@
+class Main {
+  public static void main(String[] args) {
+    //starting point
+    // System.out.println("make a variable");
+    //  String message = "Weclome to Java";
+    // System.out.println(message);
+    //   message = "Welcome to Java";
+    // System.out.println(message);
+    // Variable types:
+    // int myInteger = 19;
+
+    // //double
+    // double myDouble = 12.19;
+    // //boolean
+    // boolean myBoolean = true;
+    // boolean myOtherBoolean = false;
+    // //string
+    // String myString = "String";
+    // //char: single quotes, single character
+    // char myChar = '1';
+    // MATH OPERATORS
+    // int var1 = 10;
+    // int var2 = 6;
+
+    // int var3 = (var1 * var2) + 1;
+    // System.out.println(var3);
+    // STRING UTILITY METHODS
+    // String name = "Bob Jones";
+    // String message = "My name is " + name;
+    // String message2 = name.concat(" is an amazing person");
+    // System.out.println(message);
+    // System.out.println(message2);
+    // System.out.println(name.toLowerCase());
+    // System.out.println(name.toUpperCase());
+    // boolean areEqual = name.equals("Something Else");
+    // boolean areEqual2 = name.equals("Rob Ryan");
+    // int characterLength = name.length();
+    // System.out.println(areEqual);
+    // System.out.println(areEqual2);
+    // System.out.println(characterLength);
+      boolean run = false;
+    if (run) { 
+     System.out.println("WE MADE IT INTO THE if STATEMENT");
+      
+    }
+    System.out.println("END OF PROGRAM");
+  }
+}
